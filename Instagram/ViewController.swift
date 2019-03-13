74f700beb5d20ca0dce53d99e95b4433422d980c
mizuki.nakajima <m173305s@st.u-gakugei.ac.jp>
2019-03-13 18:07:49 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Nakajima Mizuki on 2019/03/13.
+//  Copyright © 2019 Nakajima Mizuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
