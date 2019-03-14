a870802cf2be0b69fd499960eba99947c5051c1f
mizuki.nakajima <m173305s@st.u-gakugei.ac.jp>
2019-03-14 14:01:01 +0900

    基本機能の実装

--- Instagram/PostViewController.swift
+++ Instagram/PostViewController.swift
@@ -1,10 +1,12 @@
 //
-//  PostViewViewController.swift
+//  PostViewController.swift
 //  Instagram
 //
 //  Created by Nakajima Mizuki on 2019/03/13.
 //  Copyright © 2019 Nakajima Mizuki. All rights reserved.
 //
+
+//コメントびゅー
 
 import UIKit
 import Firebase
@@ -19,10 +21,30 @@ class PostViewController: UIViewController {
     
     // 投稿ボタンをタップしたときに呼ばれるメソッド
     @IBAction func handlePostButton(_ sender: Any) {
+        // ImageViewから画像を取得する
+        let imageData = imageView.image!.jpegData(compressionQuality: 0.5)
+        let imageString = imageData!.base64EncodedString(options: .lineLength64Characters)
+        
+        // postDataに必要な情報を取得しておく
+        let time = Date.timeIntervalSinceReferenceDate
+        let name = Auth.auth().currentUser?.displayName
+        
+        // 辞書を作成してFirebaseに保存する
+        let postRef = Database.database().reference().child(Const.PostPath)
+        let postDic = ["caption": textField.text!, "image": imageString, "time": String(time), "name": name!]
+        postRef.childByAutoId().setValue(postDic)
+        
+        // HUDで投稿完了を表示する
+        SVProgressHUD.showSuccess(withStatus: "投稿しました")
+        
+        // 全てのモーダルを閉じる
+        UIApplication.shared.keyWindow?.rootViewController?.dismiss(animated: true, completion: nil)
     }
     
     // キャンセルボタンをタップしたときに呼ばれるメソッド
     @IBAction func handleCancelButton(_ sender: Any) {
+        // 画面を閉じる
+        dismiss(animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
